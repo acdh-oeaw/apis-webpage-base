b34e6c2f21b82299109ac7f4fecccbd5142465a9
Matthias Schlögl <m.schloegl@gmail.com>
2019-02-13 16:26:04 +0100

    metadata to base

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -2,22 +2,14 @@
 # (by default) in the base.html and index.html
 
 PROJECT_METADATA = {
-    'title': 'APIS',
+    'title': 'TITLE',
     'author': 'Matthias Schlögl, Peter Andorfer',
-    'subtitle': 'Austrian Prosopographical Information System',
-    'description': """
-    Das Österreichische Biographische Lexikon (ÖBL) ist das einzige enzyklopädische Werk in Mitteleuropa, das Lebens- und Karriereverläufe verdienter Persönlichkeiten des gesamten Gebiets der ehemaligen österreichisch-ungarischen Monarchie sowie der Ersten/Zweiten Republik erfasst. Basierend auf den rund 18.000 publizierten und bereits digital verfügbaren Biographien werden in Zusammenarbeit mit dem Austrian Centre for Digital Humanities (ACDH) und dem Institut für Stadt- und Regionalforschung (ISR) durch den Einsatz unterschiedlicher Methoden der Computerlinguistik (differenzierte, textlinguistische Erschließung), moderner IT-Strukturen und des Semantic Web Möglichkeiten geschaffen, um Lösungsansätze für Forschungsfragen im geistes-, sozial- und kulturwissenschaftlichen Kontext zu unterstützen und spezifische Auswertungen zu ermöglichen. Das ÖBL wird durch Transformation in eine adäquate Forschungsinfrastruktur maschinenlesbar (RDF, SKOS), semantisch angereichert, in der Linked Open Data Cloud publiziert (Open-Access Bereitstellung der Daten unter der CC-BY-NC Lizenz) sowie vollständig an internationale Standards angebunden (GND, VIAF); eine Integration in bestehende europäische Initiativen (DARIAH, CLARIN, EUROPEANA) ist projektiert. Dadurch wird zugleich die maximale Zugänglichkeit der Forschungsdaten erreicht.
-
-Im Sinne transdisziplinärer Zusammenarbeit verbindet sich die Expertise des ÖBL (historisch-biographische Forschung) mit jener des ACDH im Bereich der Corpuslinguistik und Textechnologie zur Entwicklung innovativer Methoden, um beispielhaft konkreten sozialwissenschaftlichen Forschungsfragen des ISR eine neue Plattform zu bieten; parallel wird eine umfangreiche sozialwissenschaftliche und demographische Analyse der Wanderungsformen und Wanderungsmuster gesellschaftlicher Eliten im 19. und frühen 20. Jahrhundert mit besonderer Berücksichtigung der politischen Brüche innerhalb dieses Zeitraums erarbeitet/publiziert.
-
-Das Projekt schlägt damit die Brücke von den Geisteswissenschaften über die Texttechnologie und Semantic Web-Ansätze zu den Sozialwissenschaften; es ist modellhaft, zukunftsweisend und realisiert Interdisziplinarität anhand eines konkreten „Werkstücks“.
-    """,
+    'subtitle': '',
+    'description': """""",
     'github': 'https://github.com/acdh-oeaw/apis-webpage-base',
     'production instance': None,
     'purpose_de': '',
-    'purpose_en': """This is a web application to collect, curate, enrich, analyze,
-    visualize, and export prosopgraphical and biographical data. This web application
-    is developed in the context of the APIS-Project.""",
+    'purpose_en': """""",
     'version': ['apis_core', 'charts', 'django'],
     'matomo_id': '',
     'matomo_url': '',
