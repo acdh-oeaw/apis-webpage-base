2ce79c709890fff5a35065afcd38ed3d5552a7db
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-03-06 17:37:03 +0100

    added PROJECT setting

--- apis/settings/base.py
+++ apis/settings/base.py
@@ -261,3 +261,46 @@ APIS_IIIF_ENT_IIIF_REL = "has iiif image"
 APIS_IIIF_SERVER = "https://iiif.acdh.oeaw.ac.at/"
 APIS_OSD_JS = "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/2.4.0/openseadragon.min.js"
 APIS_OSD_IMG_PREFIX = "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/2.4.0/images/"
+
+PROJECTS = {
+    'STB':
+        {
+            'abbr': 'STB',
+            'title': 'Schnitzler Diary',
+            'resolver_person': 'pages/person-detail.html?personID=',
+            'resolver_place': 'pages/place-detail.html?placeID=',
+            'base_url': 'https://schnitzler-tagebuch.acdh.oeaw.ac.at',
+        },
+    'HBAS':
+        {
+            'abbr': 'HBAS',
+            'title': 'Die Korrespondenz Hermann Bahr – Arthur Schnitzler',
+            'resolver_person': 'register.html?key=',
+            'resolver_place': 'register.html?key=',
+            'base_url': 'https://bahrschnitzler.acdh.oeaw.ac.at'
+        },
+    'FK':
+        {
+            'abbr': 'FK',
+            'title': 'Die Fackel',
+            'resolver_person': 'pages/person-detail.html?personID=',
+            'resolver_place': 'pages/place-detail.html?placeID=',
+            'base_url': 'https://fackel.acdh.oeaw.ac.at',
+        },
+    'BR':
+        {
+            'abbr': 'Brenner',
+            'title': 'Der Brenner',
+            'resolver_person': 'pages/person-detail.html?personID=',
+            'resolver_place': 'pages/place-detail.html?placeID=',
+            'base_url': 'https://brenner.acdh.oeaw.ac.at',
+        },
+    'ASBW':
+        {
+            'abbr': 'ASBW',
+            'title': 'Arthur Schnitzler Briefwechsel mit Autorinnen und Autoren',
+            'resolver_person': 'register.html?key=',
+            'resolver_place': 'register.html?key=',
+            'base_url': 'https://schnitzler-briefe.acdh.oeaw.ac.at'
+        },
+}
