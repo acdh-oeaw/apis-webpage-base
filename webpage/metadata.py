09c4ca10ec4f3d4e8f6500df705a7c77b6cd2712
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-03-21 14:00:36 +0100

    initial commit for paas branch

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -2,19 +2,17 @@
 # (by default) in the base.html and index.html
 
 PROJECT_METADATA = {
-    'title': 'PMB',
+    'title': 'PAAS',
     'author': 'Matthias Schlögl, Peter Andorfer',
-    'subtitle': 'Personen der Moderne Basis',
-    'description': """
-    Durch die Normdatei PMB wird ein Webservice für Personen, Werke, Institutionen, Orte und\
-    Ereignisse speziell für die Zeit in Wien um 1900 zur Verfügung gestellt.
-    """,
-    'github': 'https://github.com/acdh-oeaw/apis-webpage-base',
-    'production instance': True,
-    'purpose_de': 'Ziel dieser Applikation ist die Publikation von Forschungsdaten',
-    'purpose_en': """Purpose of this application is the publication of research data""",
-    'version': ['apis_core', 'charts', 'django'],
-    'matomo_id': '124',
+    'subtitle': 'Prosopography of the Members of the Austrian Academy of Sciences 1847–2022',
+    'description': 'PAAS is a web application to collect, analyze and evaluate prosopographical\
+    and biographical data of the approximately 3000 members of the\
+    Austrian Academy of Sciences 1847–2022.',
+    'github': 'https://github.com/acdh-oeaw/paas',
+    'purpose_de': 'Ziel von PAAS ist die Publikation von Forschungsdaten.',
+    'purpose_en': 'The purpose of PAAS is the publication of research data.',
+    'version': '0.0.1',
+    'matomo_id': '23',
     'matomo_url': '//matomo.acdh.oeaw.ac.at/',
     'imprint': '/imprint',
     'social_media': [
