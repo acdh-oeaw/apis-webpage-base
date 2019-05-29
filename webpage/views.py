55e2cd26a39392845b79ea8a55f0a20af1151aef
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-05-29 14:22:16 +0200

    fixed log-out view

--- webpage/views.py
+++ webpage/views.py
@@ -89,7 +89,7 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
-    return render_to_response("webpage/user_logout.html")
+    return render(request, "webpage/user_logout.html")
 
 
 def handler404(request, exception):
