7c6d5b953b97173596db7c56374384f56c1eb808
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-06-21 08:23:26 +0200

    if there si a custom theme -> use it

--- apis/urls.py
+++ apis/urls.py
@@ -4,13 +4,23 @@ from django.conf import settings
 from rest_framework import routers
 
 
-urlpatterns = [
-    url(r'^apis/', include('apis_core.urls', namespace="apis")),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^info/', include('infos.urls', namespace='info')),
-    url(r'^', include('webpage.urls', namespace='webpage')),
-]
+if 'theme' in settings.INSTALLED_APPS:
+    urlpatterns = [
+        url(r'^apis/', include('apis_core.urls', namespace="apis")),
+        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+        url(r'^admin/', admin.site.urls),
+        url(r'^info/', include('infos.urls', namespace='info')),
+        url(r'^', include('theme.urls', namespace='theme')),
+        url(r'^webpage/', include('webpage.urls', namespace='webpage')),
+    ]
+else:
+    urlpatterns = [
+        url(r'^apis/', include('apis_core.urls', namespace="apis")),
+        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+        url(r'^admin/', admin.site.urls),
+        url(r'^info/', include('infos.urls', namespace='info')),
+        url(r'^', include('webpage.urls', namespace='webpage')),
+    ]
 
 
 handler404 = 'webpage.views.handler404'
