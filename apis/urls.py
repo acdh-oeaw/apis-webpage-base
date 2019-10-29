2989ba9a81c801179d48f0259f94467036af9523
Matthias Schlögl <m.schloegl@gmail.com>
2019-10-29 16:16:38 +0100

    including new urls for theme app

--- apis/urls.py
+++ apis/urls.py
@@ -17,5 +17,15 @@ if 'transkribus' in settings.INSTALLED_APPS:
         url(r'^transkribus/', include('transkribus.urls', namespace="trp")),
     ] + urlpatterns
 
-
+if 'theme' in settings.INSTALLED_APPS:
+    urlpatterns = [
+        url(r'^apis/', include('apis_core.urls', namespace="apis")),
+        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+        url(r'^admin/', admin.site.urls),
+        url(r'^info/', include('infos.urls', namespace='info')),
+        url(r'^', include('theme.urls', namespace='theme')),
+        url(r'^webpage/', include('webpage.urls', namespace='webpage')),
+    ]
+if 'haystack' in settings.INSTALLED_APPS:
+    urlpatterns = urlpatterns + [url(r'^search/', include('haystack.urls')), ]
 handler404 = 'webpage.views.handler404'
